82317aac0e911fcff625b43bfe82422b4a86c648
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2020-04-30 00:11:30 +0200

    Export new types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { EVE } from './eve.helper';
 export * from './interface.helper';
+export { Ore } from './types/ore';
+export { Gas } from './types/gas';
+export { Mineral } from './types/mineral';
